de721db515c7f00d95910a361f7022b33c078eee
Jonas Auer <jonas.auer.94@gmail.com>
2017-06-27 22:34:09 +0200

    Replace Hellmouth Sprite with Character in head scene

--- src/states/head.ts
+++ src/states/head.ts
@@ -1,11 +1,18 @@
 import * as Assets from '../assets'
 import PnCState from './pnc-state'
+
+import HellmouthCharacter from '../characters/hellmouth'
+
+import SheechHelper from '../utils/speechHelper'
+
 import ArrayUtils from '../utils/arrayUtils'
 import StringUtils from '../utils/stringUtils'
 
 export default class HeadSceneState extends PnCState {
   protected hellmouthSprite: Phaser.Sprite = null
-  protected hellmouthSamples: string[] = []
+  protected hellmouthSamples: string[] = ArrayUtils.range(1, 38).map(i =>
+    Assets.Audio[`AudioHellmouth${StringUtils.intToString(i, 3)}`].key
+  )
 
   protected antonius: Phaser.Sprite = null
 
@@ -21,49 +28,29 @@ export default class HeadSceneState extends PnCState {
   public create(): void {
     super.create()
 
-    this.hellmouthSprite = this.game.add.sprite(135, 40, Assets.Spritesheets.SpritesheetsHellmouthTalkcycle.key)
-    this.hellmouthSprite.animations.add('talk')
-    this.hellmouthSprite.inputEnabled = true
-    this.hellmouthSprite.input.useHandCursor = true
-    this.hellmouthSprite.events.onInputDown.add(() => {
-      this.hellmouthSprite.inputEnabled = false
-      this.game.canvas.style.cursor = 'default'
-
-      this.hellmouthSprite.animations.play('talk', 16, true)
-
-      let talkCount: number = 0
-      playRandomSound(this.hellmouthSamples, () => talkCount++ < 3, () => {
-        this.hellmouthSprite.inputEnabled = true
-        this.hellmouthSprite.input.useHandCursor = true
-        this.hellmouthSprite.animations.stop()
-        this.hellmouthSprite.frame = 0
-      })
-    })
-
-    this.hellmouthSamples = ArrayUtils.range(1, 38).map(i =>
-      Assets.Audio[`AudioHellmouth${StringUtils.intToString(i, 3)}`].key
-    )
+    const hellmouth = new HellmouthCharacter(this.game, 135, 40)
+    this.game.add.existing(hellmouth)
 
     const self = this
-    function playRandomSound(sounds: string[], playWhile: () => boolean, done = (() => {}) , lastSound?: string) {
-      const availableSounds = lastSound ? sounds.slice(0, sounds.indexOf(lastSound)).concat(sounds.slice(sounds.indexOf(lastSound) + 1)) : sounds
-      const nextSound = Phaser.ArrayUtils.getRandomItem(availableSounds)
-      self.game.sound.play(nextSound).onStop.addOnce(() => {
-        if (playWhile()) {
-          playRandomSound(sounds, playWhile, done, nextSound)
-        } else {
-          done()
-        }
+    function makeMouthTalk() {
+      hellmouth.inputEnabled = true
+      hellmouth.input.useHandCursor = true
+      hellmouth.events.onInputDown.addOnce(() => {
+        hellmouth.inputEnabled = false
+        self.game.canvas.style.cursor = 'default'
+        hellmouth.speech.say('hello', 2, null, async () => {
+          hellmouth.setActiveState('talking')
+        })
+        .then(() => hellmouth.setActiveState('idle'))
+        .then(makeMouthTalk)
       })
     }
+    makeMouthTalk()
 
     this.antonius = this.game.add.sprite(258, 120, Assets.Spritesheets.SpritesheetsAntoniusTalkcycle.key)
     this.antonius.scale = new Phaser.Point(2, 2)
 
+    // Fade in from black over one second
     this.game.camera.flash(0x000000, 1000)
   }
-}
-
-class HeadSceneIntroState extends HeadSceneState {
-
 }
