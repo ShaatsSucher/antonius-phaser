2b95f12e262fd71d40ad99addbad2e684d9ef99e
Jonas Auer <jonas.auer.94@gmail.com>
2017-06-27 22:48:14 +0200

    Tidy up head scene

--- src/states/head.ts
+++ src/states/head.ts
@@ -8,27 +8,14 @@ import ArrayUtils from '../utils/arrayUtils'
 import StringUtils from '../utils/stringUtils'
 
 export default class HeadSceneState extends Phaser.State {
-  protected hellmouthSprite: Phaser.Sprite = null
-  protected hellmouthSamples: string[] = ArrayUtils.range(1, 38).map(i =>
-    Assets.Audio[`AudioHellmouth${StringUtils.intToString(i, 3)}`].key
-  )
-
-  protected antonius: Phaser.Sprite = null
-
-
-  constructor() {
-    super(Assets.Images.ImagesBackgroundHead.key)
-  }
-
-  public init(): void {
-
-  }
+  private hellmouth: HellmouthCharacter = null
 
   public create(): void {
     // Add background
     this.game.add.sprite(0, 0, Assets.Images.ImagesBackgroundHead.key)
 
-    const hellmouth = new HellmouthCharacter(this.game, 135, 40)
+    // Add hellmouth
+    const hellmouth = this.hellmouth = new HellmouthCharacter(this.game, 135, 40)
     this.game.add.existing(hellmouth)
 
     const self = this
@@ -47,8 +34,9 @@ export default class HeadSceneState extends Phaser.State {
     }
     makeMouthTalk()
 
-    this.antonius = this.game.add.sprite(258, 120, Assets.Spritesheets.SpritesheetsAntoniusTalkcycle.key)
-    this.antonius.scale = new Phaser.Point(2, 2)
+    // Add antonius
+    const antonius = this.game.add.sprite(258, 120, Assets.Spritesheets.SpritesheetsAntoniusTalkcycle.key)
+    antonius.scale = new Phaser.Point(2, 2)
 
     // Fade in from black over one second
     this.game.camera.flash(0x000000, 1000)
