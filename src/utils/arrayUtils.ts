2dc6989a97cd4a63610d3c717d5705b35ea822e7
Jonas Auer <jonas.auer.94@gmail.com>
2017-06-27 22:30:04 +0200

    Add removeAt and removeItem to ArrayUtils

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -7,4 +7,13 @@ export default class ArrayUtils {
     let counter = min
     return Array.apply(null, { length: max - min + 1 }).map(() => counter++)
   }
+
+  public static removeAt<T>(array: T[], index: number) {
+    return array.slice(0, index).concat(array.slice(index + 1))
+  }
+
+  public static removeItem<T>(array: T[], item: T) {
+    const index = array.indexOf(item)
+    return index === -1 ? array : ArrayUtils.removeAt(array, index)
+  }
 }
