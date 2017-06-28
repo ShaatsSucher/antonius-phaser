22155119c8d866683b8957e9d9ab38680f00b10b
Jonas Auer <jonas.auer.94@gmail.com>
2017-06-28 12:15:56 +0200

    Add Arrow game object

---
+++ src/gameObjects/arrow.ts
@@ -0,0 +1,59 @@
+import { Spritesheets } from '../assets'
+
+export default class Arrow extends Phaser.Sprite {
+  private _enabled = false
+  private hovered = false
+
+  constructor(game: Phaser.Game, x: number, y: number) {
+    super(game, x, y, Spritesheets.SpritesheetsArrow.key)
+
+    this.animations.add('default', [0], 1, false)
+    this.animations.add('hovered', [1], 1, false)
+
+    this.enabled = true
+
+    this.anchor = new Phaser.Point(0.5, 0.5)
+
+    this.events.onInputOver.add(this.onInputOver, this)
+    this.events.onInputOut.add(this.onInputOut, this)
+  }
+
+  public get enabled(): boolean {
+    return this._enabled
+  }
+
+  public set enabled(value: boolean) {
+    if (value === this.enabled) return
+    this._enabled = value
+    if (this.enabled) {
+      this.inputEnabled = true
+      this.input.useHandCursor = true
+      this.play('blinking')
+      this.alpha = 1
+    } else {
+      this.inputEnabled = false
+      if (this.hovered) {
+        this.game.canvas.style.cursor = 'default'
+      }
+      this.play('static')
+      this.alpha = 0.6
+    }
+    this.updateTexture()
+  }
+
+  private onInputOver() {
+    if (this.hovered) return
+    this.hovered = true
+    this.updateTexture()
+  }
+
+  private onInputOut() {
+    if (!this.hovered) return
+    this.hovered = false
+    this.updateTexture()
+  }
+
+  private updateTexture() {
+    this.play(this.enabled && this.hovered ? 'hovered' : 'default')
+  }
+}
