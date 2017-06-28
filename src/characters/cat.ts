77ea46578b198ea3a71e43b4a3e94981c4d8e4d9
Jonas Auer <jonas.auer.94@gmail.com>
2017-06-28 12:45:53 +0200

    Add Cat Character

---
+++ src/characters/cat.ts
@@ -0,0 +1,45 @@
+import * as Assets from '../assets'
+import Character from './character'
+import CharacterState from './characterState'
+import ArrayUtils from '../utils/arrayUtils'
+import StringUtils from '../utils/stringUtils'
+import SpeechHelper from '../utils/speechHelper'
+
+export default class CatCharacter extends Character {
+  public speech = new SpeechHelper(this, 0, 0, SpeechHelper.Generators.sequential(
+    ArrayUtils.range(1, 3).map(i =>
+      Assets.Audio[`AudioCatDenial${StringUtils.intToString(i, 3)}`].key
+    )
+  ))
+
+  constructor(game: Phaser.Game, x: number, y: number) {
+    super(game, x, y, Assets.Spritesheets.SpritesheetsCat.key)
+
+    this.animations.add('idle', null, 6, true)
+
+    this.addCharacterState('idle', new IdleState(this))
+    this.addCharacterState('talking', new TalkingState(this))
+
+    this.play('idle')
+  }
+}
+
+class IdleState implements CharacterState<CatCharacter> {
+  constructor(public character: CatCharacter) { }
+
+  async enter() {
+    this.character.play('idle')
+    this.character.animations.currentAnim.loop = true
+    this.character.animations.currentAnim.speed = 3
+  }
+}
+
+class TalkingState implements CharacterState<CatCharacter> {
+  constructor(public character: CatCharacter) { }
+
+  async enter() {
+    this.character.play('idle')
+    this.character.animations.currentAnim.loop = true
+    this.character.animations.currentAnim.speed = 6
+  }
+}
--- src/states/scenes/bardScene.ts
+++ src/states/scenes/bardScene.ts
@@ -3,6 +3,7 @@ import * as Assets from '../../assets'
 import AntoniusCharacter from '../../characters/antonius'
 import BardCharacter from '../../characters/bard'
 import GooseCharacter from '../../characters/goose'
+import CatCharacter from '../../characters/cat'
 
 import SheechHelper from '../../utils/speechHelper'
 
@@ -12,6 +13,8 @@ import StringUtils from '../../utils/stringUtils'
 export default class BardScene extends Phaser.State {
   private goose: GooseCharacter
   private bard: BardCharacter
+  private cat: CatCharacter
+
   private antonius: AntoniusCharacter
 
   public create(): void {
@@ -26,13 +29,19 @@ export default class BardScene extends Phaser.State {
     bard.scale = new Phaser.Point(3, 3)
     this.game.add.existing(bard)
 
+    const cat = this.cat = new CatCharacter(this.game, 48, 10)
+    cat.scale = new Phaser.Point(3, 3)
+    this.game.add.existing(cat)
+
     const antonius = this.antonius = new AntoniusCharacter(this.game, 292, 120)
     antonius.scale = new Phaser.Point(3, 3)
     this.game.add.existing(antonius)
 
-    bard.setActiveState('walking')
-    goose.setActiveState('walking')
-    antonius.setActiveState('talking')
+    bard.setActiveState('idle')
+    goose.setActiveState('idle')
+    cat.setActiveState('idle')
+
+    antonius.setActiveState('idle')
 
     // Fade in from black over one second
     this.game.camera.flash(0x000000, 1000)
