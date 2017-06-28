48b5337bba2fedb66b92ca5ef92f7171309be894
Jonas Auer <jonas.auer.94@gmail.com>
2017-06-28 17:04:48 +0200

    Remove remnants of the TalkingCharacterState

--- src/characters/characterState.ts
+++ src/characters/characterState.ts
@@ -7,24 +7,3 @@ interface CharacterState<T extends Character> {
   enter?(): Promise<void>
   exit?(): Promise<void>
 }
-
-export class TalkingCharacterState<T extends Character> implements CharacterState<T> {
-  private activeSound: Phaser.Sound = null
-  public text = ''
-  public duration = 0
-  private beginTalkingTimestamp = 0
-
-  constructor(public character: T,
-              private talkSamples: string[],
-              private doneTalking: () => { }) { }
-
-  async enter() {
-
-  }
-
-  async exit() {
-    if (this.activeSound && (this.activeSound.isPlaying || this.activeSound.pendingPlayback)) {
-      this.activeSound.stop()
-    }
-  }
-}
