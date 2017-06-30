beccf8fb88aaca1abbd1d0f165f0066d7a30ec61
Jonas Auer <jonas.auer.94@gmail.com>
2017-06-30 19:28:49 +0200

    Add open and closed mouth states to the hellmouth

--- src/characters/hellmouth.ts
+++ src/characters/hellmouth.ts
@@ -14,11 +14,15 @@ export default class HellmouthCharacter extends Character {
   constructor(game: Phaser.Game, x: number, y: number) {
     super(game, x, y, Assets.Spritesheets.hellmouthTalkcycle.key)
 
-    this.animations.add('idle', [0], 60, true)
+    this.animations.add('idle', [0], 0, false)
     this.animations.add('talking', null, 16, true)
+    this.animations.add('open mouth', ArrayUtils.range(0, 9), 16, false)
+    this.animations.add('close mouth', ArrayUtils.range(10, 17), 16, false)
 
     this.addCharacterState('idle', new IdleState(this))
     this.addCharacterState('talking', new TalkingState(this))
+    this.addCharacterState('open mouth', new OpenMouthState(this))
+    this.addCharacterState('close mouth', new CloseMouthState(this))
 
     this.play('idle')
   }
@@ -38,5 +42,30 @@ class TalkingState implements CharacterState<HellmouthCharacter> {
   async enter() {
     this.character.animations.stop() // reset animation if necessary
     const anim = this.character.play('talking')
+  }
+}
+
+class OpenMouthState implements CharacterState<HellmouthCharacter> {
+  constructor(public character: HellmouthCharacter) { }
+
+  async enter() {
+    this.character.animations.stop()
+    const anim = this.character.play('open mouth')
+    let animDoneCb
+    const animDone = new Promise(resolve => { animDoneCb = resolve })
+    anim.onComplete.addOnce(animDoneCb)
+    await animDone
+  }
+}
+
+class CloseMouthState implements CharacterState<HellmouthCharacter> {
+  constructor(public character: HellmouthCharacter) { }
+
+  async enter() {
+    this.character.animations.stop()
+    const anim = this.character.play('close mouth')
+    anim.onComplete.addOnce(() => {
+      this.character.setActiveState('idle')
+    })
   }
 }
