6189665e4c5ef09fe42d797e2081ba97f1b720c5
Jonas Auer <jonas.auer.94@gmail.com>
2017-07-11 23:06:49 +0200

    Make Inventory capable of holding <= 1 items

--- src/overlays/inventory.ts
+++ src/overlays/inventory.ts
@@ -25,12 +25,35 @@ export default class Inventory extends Phaser.Group {
   }
 
   /* Actual Class */
+
+  private readonly inventoryItem: Phaser.Sprite
+  public get item(): string {
+    return <string>this.inventoryItem.key
+  }
+  public set item(value: string) {
+    this.inventoryItem.loadTexture(value)
+    this.inventoryItem.visible = !!value
+
+    this.inventoryItem.position.setTo(
+      Math.floor(this.background.width / 2),
+      Math.floor(this.background.y - this.background.height / 2)
+    )
+    if (this.inventoryItem.width % 2 !== 0) this.inventoryItem.x += 0.5
+    if (this.inventoryItem.height % 2 !== 0) this.inventoryItem.y += 0.5
+  }
+
+  private readonly background: Phaser.Sprite
+
   private constructor(game: Phaser.Game) {
     super(game, null, 'inventory', true)
     this.visible = false
 
-    const background = new Phaser.Sprite(game, 0, this.game.height, Images.uiInventoryBackground.key)
-    this.add(background)
-    background.anchor.setTo(0, 1)
+    this.background = new Phaser.Sprite(game, 0, this.game.height, Images.uiInventoryBackground.key)
+    this.add(this.background)
+    this.background.anchor.setTo(0, 1)
+
+    this.inventoryItem = new Phaser.Sprite(game, 0, 0)
+    this.inventoryItem.anchor.setTo(0.5)
+    this.add(this.inventoryItem)
   }
 }
