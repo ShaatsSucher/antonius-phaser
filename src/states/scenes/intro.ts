f58ca5a9c73455cc9a980b67618dd475e6283941
Jonas Auer <jonas.auer.94@gmail.com>
2017-07-11 17:07:18 +0200

    Add intro animation and sound

--- src/states/scenes/intro.ts
+++ src/states/scenes/intro.ts
@@ -1,23 +1,24 @@
 import Scene from './scene'
 import SceneState from './sceneState'
 
-import * as Assets from '../../assets'
+import { Images, Audio } from '../../assets'
 
 import { ArrayUtils, StringUtils } from '../../utils/utils'
 
 export default class IntroScene extends Scene {
-  startButton: Phaser.Text
+  image: Phaser.Sprite
 
   constructor() {
     super('', InitialState)
   }
 
   protected createGameObjects() {
-    this.startButton = this.add.text(this.world.centerX, this.world.centerY,
-      'Insert intro here', {
-        fill: 'white'
-      })
-    this.startButton.anchor = new Phaser.Point(0.5, 0.5)
+    this.image = this.add.sprite(
+      Math.round(this.world.width * 0.292),
+      Math.round(this.world.height * 0.426),
+      Images.backgroundsCraesbeeckAntonius.key
+    )
+    this.image.anchor.setTo(0.294, 0.489)
   }
 }
 
@@ -25,11 +26,28 @@ class InitialState implements SceneState<IntroScene> {
   constructor(public readonly scene: IntroScene) { }
   public getStateName() { return 'initial' }
 
+  private audio: Phaser.Sound
+
   public async enter(): Promise<void> {
-    this.scene.startButton.inputEnabled = true
-    this.scene.startButton.input.useHandCursor = true
-    this.scene.startButton.events.onInputDown.addOnce(() => {
+    const worldRightOfPosition = this.scene.game.canvas.width - this.scene.image.x
+    const imageRightOfAnchor = this.scene.image.width - this.scene.image.width * this.scene.image.anchor.x
+    const initialScale = worldRightOfPosition / imageRightOfAnchor
+    this.scene.image.scale.setTo(initialScale)
+    this.scene.image.tint = 0xaaaaaa
+
+    const updateHandle = this.scene.onUpdate.add(() => {
+      const progress = this.audio.currentTime / this.audio.durationMS
+      this.scene.image.scale.setTo(initialScale + progress * (1 - initialScale))
+    })
+
+    this.audio = this.scene.sound.play(Audio.introIntro.key)
+    this.audio.onStop.addOnce(() => {
+      updateHandle.detach()
       this.scene.fadeTo('head')
     })
+  }
+
+  public async exit(): Promise<void> {
+    this.audio.stop()
   }
 }
