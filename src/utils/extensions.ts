3a394afd2553ae57bf31dfe6a8f369015dcd59fb
Jonas Auer <jonas.auer.94@gmail.com>
2017-08-27 21:57:14 +0200

    Add flatMap method to Array prototype

--- src/utils/extensions.ts
+++ src/utils/extensions.ts
@@ -10,3 +10,12 @@ Phaser.Signal.prototype.asPromise = function asPromise() {
     this.addOnce(resolve)
   })
 }
+
+interface Array<T> {
+  flatMap<U>(callback: (currentValue: T, index: number, array: T[]) => U): U[]
+}
+Array.prototype.flatMap = function flatMap<U>(callback: (currentValue, index: number, array: any[]) => U): U[] {
+  return this.map(callback).filter(value =>
+    value !== undefined && value !== null
+  )
+}
