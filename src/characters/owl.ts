dd0c7a3f0b3d5e2afff74a5972aa1ae3fafd77d0
Jonas Auer <jonas.auer.94@gmail.com>
2017-10-20 01:35:36 +0200

    Add proper state to OwlCharacter

--- src/characters/owl.ts
+++ src/characters/owl.ts
@@ -9,10 +9,20 @@ export default class OwlCharacter extends Character {
   public readonly speech = new SpeechHelper(this, 0, 0, SpeechHelper.Generators.mute())
 
   constructor(scene: Scene, x: number, y: number) {
-    super(scene, x, y, Assets.Spritesheets.peeingowl.key)
+    super(scene, x, y, Assets.Spritesheets.peeingowl.key, null, 'peeing')
 
     this.animations.add('peeing', ArrayUtils.range(0, 7), 8, true)
 
+    this.addCharacterState('peeing', new PeeingState(this))
+
     this.play('peeing')
+  }
+}
+
+class PeeingState implements CharacterState<OwlCharacter> {
+  constructor(public character: OwlCharacter) {}
+
+  async enter() {
+    this.character.play('peeing')
   }
 }
