f7a6e430dfd117f22dc456501220254ca88ff739
Jonas Auer <jonas.auer.94@gmail.com>
2017-10-19 10:29:51 +0200

    Change bard song to be played on the speech track
    Fix #135

--- src/states/scenes/bard.ts
+++ src/states/scenes/bard.ts
@@ -177,8 +177,8 @@ class BardConversation extends SceneStateTransition<BardScene> {
 
     scene.characters.bard.setActiveState('singing')
     scene.characters.bard.interactionEnabled = true
-    const bardSong = scene.sound.play(Audio.bardSongShort.key)
-    bardSong.onStop.addOnce(() => { scene.characters.bard.setActiveState('idle') })
+    const bardSong = AudioManager.instance.tracks.speech.addClip(Audio.bardSongShort.key)
+    bardSong.stopped.then(() => { scene.characters.bard.setActiveState('idle') })
 
     const clickedAnywhere = new Promise<void>(resolve => {
       this.scene.game.input.mouse.capture = true
