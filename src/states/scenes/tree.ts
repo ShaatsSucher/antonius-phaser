246db9ece98a2951f36946aa9af1b79759ee502f
Jonas Auer <jonas.auer.94@gmail.com>
2017-10-20 00:47:56 +0200

    Add tree states and transitions

--- src/states/scenes/tree.ts
+++ src/states/scenes/tree.ts
@@ -1,5 +1,10 @@
 import Scene from './scene'
-import { SceneStateManager, SceneState, SceneStateTransition } from '../../utils/stateManager'
+import { SceneStateManager
+       , SceneState
+       , SceneStateTransition
+       , ConditionalStateTransition
+       , TransitionCondition
+       } from '../../utils/stateManager'
 
 import { Images, Audio } from '../../assets'
 
@@ -8,17 +13,19 @@ import TreeCharacter from '../../characters/tree'
 import WomanCharacter from '../../characters/woman'
 import GoatCharacter from '../../characters/goat'
 
+import { BardGone, MeckieGone } from './bard'
+
 import Arrow from '../../gameObjects/arrow'
 
 import Inventory from '../../overlays/inventory'
 
 export default class TreeScene extends Scene {
-  public characters = {
-    antonius: null,
-    tree: null,
-    woman: null,
-    goat: null
-  }
+  public characters: {
+    antonius: AntoniusCharacter,
+    tree: TreeCharacter,
+    woman: WomanCharacter,
+    // goat: GoatCharacter
+  } = <any>{}
 
   public interactiveObjects = {
     toBardArrow: null,
@@ -27,11 +34,28 @@ export default class TreeScene extends Scene {
     toCaveArrow: null
   }
 
-  stateManagers = {
+  stateManagers: { [name: string]: SceneStateManager<TreeScene> } = {
     default: new SceneStateManager<TreeScene>(this, [
       Initial
     ], [
 
+    ]),
+    tree: new SceneStateManager<TreeScene>(this, [
+      TreeWaitingForMeckieGone,
+      TreeReadyToTalk,
+      TreeWaitingForOwlGone,
+      TreeWaitingForAllGone,
+      AntoniusRequestedEntryForTheFirstTime,
+      TreeDeniedEntry,
+      TreeWillAllowEntry,
+      TreeAllowedEntry
+    ], [
+      TreeAllowingAscend,
+      TreeStillWaitingForOwlGone,
+      AntoniusRequestsEntryForTheFirstTime,
+      TreeDeniesEntry,
+      AntoniusRequestsEntry,
+      TreeOpeningUp
     ])
   }
 
@@ -40,6 +64,36 @@ export default class TreeScene extends Scene {
       game,
       Images.backgroundsWoman.key,
       Audio.soundscapesScene7.key
+    )
+  }
+
+  protected registerConditionalStateTransitions(scenes: { [title: string]: Scene }) {
+    const allGoneExceptTreeAndAntonius =
+      TransitionCondition.reachedState(scenes.bard.stateManagers.meckie, MeckieGone)
+        .and(TransitionCondition.reachedState(scenes.bard.stateManagers.bard, BardGone))
+        // TODO: add all *Gone-states (except the ones for people on the forehead)
+
+    this.stateManagers.tree.registerConditionalTransitions(
+      new ConditionalStateTransition(
+        TreeReadyToTalk,
+        TransitionCondition.reachedState(scenes.bard.stateManagers.meckie, MeckieGone)
+      ),
+      new ConditionalStateTransition(
+        TreeDeniesEntry,
+        TransitionCondition.reachedState(this.stateManagers.tree, AntoniusRequestedEntryForTheFirstTime)
+          .and(allGoneExceptTreeAndAntonius.not()) // TODO: make sure this only triggers <= 1 times
+      ),
+      new ConditionalStateTransition(
+        TreeOpeningUp,
+        TransitionCondition.reachedState(this.stateManagers.tree, AntoniusRequestedEntryForTheFirstTime)
+          .and(allGoneExceptTreeAndAntonius) // TODO: make sure this only triggers <= 1 times
+          .and(TransitionCondition.reachedState(this.stateManagers.tree, TreeDeniedEntry).not())
+      ),
+      new ConditionalStateTransition(
+        TreeWillAllowEntry,
+        TransitionCondition.reachedState(this.stateManagers.tree, TreeDeniedEntry)
+          .and(allGoneExceptTreeAndAntonius)
+      )
     )
   }
 
@@ -56,9 +110,9 @@ export default class TreeScene extends Scene {
     woman.scale = new Phaser.Point(3, 3)
     this.game.add.existing(woman)
 
-    const goat = this.characters.goat = new GoatCharacter(this, 280, 70)
-    goat.scale = new Phaser.Point(3, 3)
-    this.game.add.existing(goat)
+    // const goat = this.characters.goat = new GoatCharacter(this, 280, 70)
+    // goat.scale = new Phaser.Point(3, 3)
+    // this.game.add.existing(goat)
 
     const toBardArrow = this.interactiveObjects.toBardArrow = new Arrow(this.game, 20, 95)
     toBardArrow.rotation = Math.PI
@@ -100,5 +154,160 @@ export default class TreeScene extends Scene {
 export class Initial extends SceneState<TreeScene> {
   public async show() {
     const scene = this.scene
+  }
+}
+
+class TreeWaitingForMeckieGone extends SceneState<TreeScene> {
+  public async show() {
+    this.scene.interactiveObjects.toCanopyArrow.visible = false
+    this.scene.interactiveObjects.toCaveArrow.visible = false
+  }
+}
+
+class TreeReadyToTalk extends SceneState<TreeScene> {
+  public async show() {
+    this.scene.interactiveObjects.toCanopyArrow.visible = false
+    this.scene.interactiveObjects.toCaveArrow.visible = false
+
+    this.scene.characters.tree.interactionEnabled = true
+    this.listeners.push(this.scene.characters.tree.events.onInputUp.addOnce(
+      () => this.stateManager.trigger(TreeAllowingAscend)
+    ))
+  }
+}
+
+class TreeAllowingAscend extends SceneStateTransition<TreeScene> {
+  public async enter() {
+    const scene = this.scene
+
+    await scene.characters.tree.speech.say('Endlich ist der Verrückte mit dem Messer weg!', 1)
+    await scene.characters.tree.speech.say('Ich hatte solche Angst um meine Rinde!', 1)
+    await scene.characters.antonius.speech.say('Deine Rinde ist unversehrt.', null, 'sssllssl')
+    await scene.characters.tree.speech.say('Aber sie ist sowas von schrumpelig...', 1)
+    await scene.characters.antonius.speech.say('Überhaupt nicht.', null, 'ssll')
+    await scene.characters.antonius.speech.say('Ich wollte fragen, ob ich\nin deine Baumhöhle darf.', null, 'ssslssslslslls')
+    await scene.characters.tree.speech.say('Ach... Eher nicht.', 1)
+    await scene.characters.tree.speech.say('Das wäre mir doch ein bisschen unangenehm...', 1)
+    await scene.characters.antonius.speech.say('Kann ich verstehen.', null, 'slssl')
+    await scene.characters.tree.speech.say('Aber... Ich könnte deine Hilfe gebrauchen...', 1)
+
+    return TreeStillWaitingForOwlGone
+  }
+}
+
+class TreeStillWaitingForOwlGone extends SceneStateTransition<TreeScene> {
+  public async enter() {
+    const scene = this.scene
+
+    await scene.characters.tree.speech.say('Die Eule da oben...\nPisst mich einfach an.', 1)
+    await scene.characters.antonius.speech.say('Ich schaue mal, was ich da tun kann.', null, 'sslllssls')
+
+    return TreeWaitingForOwlGone
+  }
+}
+
+class TreeWaitingForOwlGone extends SceneState<TreeScene> {
+  public async show() {
+    this.scene.interactiveObjects.toCaveArrow.visible = false
+
+    this.scene.characters.tree.interactionEnabled = true
+    this.listeners.push(this.scene.characters.tree.events.onInputUp.add(
+      () => this.stateManager.trigger(TreeStillWaitingForOwlGone)
+    ))
+  }
+}
+
+class TreeWaitingForAllGone extends SceneState<TreeScene> {
+  public async show() {
+    this.scene.interactiveObjects.toCaveArrow.visible = false
+
+    this.scene.characters.tree.interactionEnabled = true
+    this.listeners.push(this.scene.characters.tree.events.onInputUp.add(
+      () => this.stateManager.trigger(AntoniusRequestsEntryForTheFirstTime)
+    ))
+  }
+}
+
+class AntoniusRequestsEntryForTheFirstTime extends SceneStateTransition<TreeScene> {
+  public async enter() {
+    const scene = this.scene
+
+    await scene.characters.antonius.speech.say('Das Eulen-Problem ist gelöst.', null, 'sslslssl')
+    await scene.characters.tree.speech.say('Antonius, mir fällt ein Stein vom Herzen!', 1)
+    await scene.characters.antonius.speech.say('Ach, das hab ich doch gerne gemacht.', null, 'ssslsslsl')
+    await scene.characters.tree.speech.say('Du hast so viel für mich getan.\nWie kann ich dir danken?', 1)
+    await scene.characters.antonius.speech.say('Dürfte ich jetzt vielleicht\ndeine Baumhöhle betreten?', null, 'ssllslssslsssl')
+    await scene.characters.tree.speech.say('Warum möchtest du das denn?', 1)
+    await scene.characters.antonius.speech.say('Ich würde wirklich gerne\nmal eine von innen sehen.', null, 'lslslslssllsssl')
+
+    return AntoniusRequestedEntryForTheFirstTime
+  }
+}
+
+class AntoniusRequestedEntryForTheFirstTime extends SceneState<TreeScene> {
+  public async enter() {
+    this.scene.interactiveObjects.toCaveArrow.visible = false
+  }
+}
+
+class TreeDeniesEntry extends SceneStateTransition<TreeScene> {
+  public async enter() {
+    const scene = this.scene
+
+    await scene.characters.tree.speech.say('Ich würde dir ja gerne diesen Gefallen tun,\naber mir ist das schrecklich unangenehm,\nwenn mich jeder dabei sieht.', 1)
+    await scene.characters.antonius.speech.say('Oh, verstehe.\nVielleicht später!', null, 'lsslsssl')
+
+    return TreeDeniedEntry
+  }
+}
+
+class TreeDeniedEntry extends SceneState<TreeScene> {
+  public async show() {
+    this.scene.interactiveObjects.toCaveArrow.visible = false
+
+    this.scene.characters.tree.interactionEnabled = true
+    this.listeners.push(this.scene.characters.tree.events.onInputUp.add(
+      () => this.stateManager.trigger(AntoniusRequestsEntry)
+    ))
+  }
+}
+
+class AntoniusRequestsEntry extends SceneStateTransition<TreeScene> {
+  public async enter() {
+    const scene = this.scene
+
+    await scene.characters.antonius.speech.say('Dürfte ich jetzt vielleicht\ndeine Baumhöhle betreten?', null, 'ssllslssslsssl')
+
+    return TreeDeniesEntry
+  }
+}
+
+class TreeWillAllowEntry extends SceneState<TreeScene> {
+  public async show() {
+    this.scene.interactiveObjects.toCaveArrow.visible = false
+
+    this.scene.characters.tree.interactionEnabled = true
+    this.listeners.push(this.scene.characters.tree.events.onInputUp.add(
+      () => this.stateManager.trigger(TreeOpeningUp)
+    ))
+  }
+}
+
+class TreeOpeningUp extends SceneStateTransition<TreeScene> {
+  public async enter() {
+    const scene = this.scene
+
+    await scene.characters.antonius.speech.say('Dürfte ich jetzt vielleicht\ndeine Baumhöhle betreten?', null, 'ssllslssslsssl')
+    await scene.characters.tree.speech.say('Wenn du das unbedingt möchtest...', 1)
+    await scene.characters.tree.speech.say('Aber ich habe nicht aufgeräumt!', 1)
+    await scene.characters.antonius.speech.say('Danke für dein Vertrauen.', null, 'ssslssl')
+
+    return TreeAllowedEntry
+  }
+}
+
+class TreeAllowedEntry extends SceneState<TreeScene> {
+  public async show() {
+    // No need to do anything, all arrows are automatically enabled
   }
 }
