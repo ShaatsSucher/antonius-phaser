fcec6289f67950c0e4555d7860adc3adc9234475
Jonas Auer <jonas.auer.94@gmail.com>
2017-10-20 02:53:11 +0200

    Complete buckethead\'s states

--- src/characters/buckethead.ts
+++ src/characters/buckethead.ts
@@ -6,30 +6,36 @@ import Scene from '../states/scenes/scene'
 import SpeechHelper from '../utils/speechHelper'
 
 export default class BucketheadCharacter extends Character {
-  public readonly speech = new SpeechHelper(this, 0, 0, SpeechHelper.Generators.combine({
-    bucket: ArrayUtils.range(1, 29).map(i =>
+  public readonly speech = new SpeechHelper(this, 0, 0,
+    SpeechHelper.Generators.random(ArrayUtils.range(1, 29).map(i =>
       Assets.Audio[`bucketmanWithBucket${StringUtils.intToString(i, 3)}`].key
-    ),
-    hat: ArrayUtils.range(1, 29).map(i =>
+    )), false, 'idleBucket', 'talkingBucket'
+  )
+
+  public readonly hatSpeech = new SpeechHelper(this, 0, 0,
+    SpeechHelper.Generators.random(ArrayUtils.range(1, 29).map(i =>
       Assets.Audio[`bucketmanNoBucket${StringUtils.intToString(i, 3)}`].key
-    )
-  }))
+    )), false, 'idleHat', 'talkingHat'
+  )
 
   constructor(scene: Scene, x: number, y: number) {
-    super(scene, x, y, Assets.Spritesheets.bucketman.key)
+    super(scene, x, y, Assets.Spritesheets.bucketman.key, null, 'idleBucket')
 
     this.animations.add('idleBucket', ArrayUtils.range(2, 13), 8, true)
     this.animations.add('idleHat', ArrayUtils.range(16, 27), 8, true)
     this.animations.add('talkingBucket', [0, 1], 8, true)
     this.animations.add('talkingHat', [14, 15], 8, true)
 
-    this.addCharacterState('idleBuckert', new IdleBucketState(this))
+    this.addCharacterState('idleBucket', new IdleBucketState(this))
+    this.addCharacterState('talkingBucket', new TalkingBucketState(this))
+    this.addCharacterState('idleHat', new IdleHatState(this))
+    this.addCharacterState('talkingHat', new IdleHatState(this))
 
     this.play('idleBucket')
   }
 }
 
-class IdleBucketState implements  CharacterState<BucketheadCharacter> {
+class IdleBucketState implements CharacterState<BucketheadCharacter> {
   constructor(public character: BucketheadCharacter) {}
 
   async enter() {
@@ -38,7 +44,7 @@ class IdleBucketState implements  CharacterState<BucketheadCharacter> {
   }
 }
 
-class TalkingBucketState implements  CharacterState<BucketheadCharacter> {
+class TalkingBucketState implements CharacterState<BucketheadCharacter> {
   constructor(public character: BucketheadCharacter) {}
 
   async enter() {
@@ -47,7 +53,7 @@ class TalkingBucketState implements  CharacterState<BucketheadCharacter> {
   }
 }
 
-class IdleHatState implements  CharacterState<BucketheadCharacter> {
+class IdleHatState implements CharacterState<BucketheadCharacter> {
   constructor(public character: BucketheadCharacter) {}
 
   async enter() {
@@ -56,7 +62,7 @@ class IdleHatState implements  CharacterState<BucketheadCharacter> {
   }
 }
 
-class TalkingHatState implements  CharacterState<BucketheadCharacter> {
+class TalkingHatState implements CharacterState<BucketheadCharacter> {
   constructor(public character: BucketheadCharacter) {}
 
   async enter() {
