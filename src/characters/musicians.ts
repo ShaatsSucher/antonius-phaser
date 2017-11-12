da4e63401c4013845e60c94f003666655b8ac052
Jonas Auer <jonas.auer.94@gmail.com>
2017-11-12 19:15:40 +0100

    Implement town musicians states Fix #100

--- src/characters/musicians.ts
+++ src/characters/musicians.ts
@@ -6,12 +6,23 @@ import Scene from '../states/scenes/scene'
 import SpeechHelper from '../utils/speechHelper'
 
 export default class MusiciansCharacter extends Character {
-  public readonly speech = new SpeechHelper(this, 0, 0, SpeechHelper.Generators.random(
-    ArrayUtils.range(1, 14).map(i =>
-      Assets.Audio[`goose${StringUtils.intToString(i, 3)}`].key
-      // TODO: replace with correct sounds
-    )
-  ))
+  public readonly speech = new SpeechHelper(this, 0, 0, SpeechHelper.Generators.combine({
+    default: SpeechHelper.Generators.random(
+      ArrayUtils.range(1, 14).map(i =>
+        Assets.Audio[`townmusicians${StringUtils.intToString(i, 3)}`].key
+      )
+    ),
+    fighting: SpeechHelper.Generators.random(
+      ArrayUtils.range(1, 4).map(i =>
+        Assets.Audio[`townmusiciansCombat${StringUtils.intToString(i, 3)}`].key
+      )
+    ),
+    distance: SpeechHelper.Generators.random(
+      ArrayUtils.range(1, 14).map(i =>
+        Assets.Audio[`townmusiciansDistance${StringUtils.intToString(i, 3)}`].key
+      )
+    ),
+  }))
 
   constructor(scene: Scene, x: number, y: number) {
     super(scene, x, y, Assets.Spritesheets.townmusicians.key)
