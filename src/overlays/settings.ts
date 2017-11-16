2c1684f114e9bb4d1cf92980c905c66b60e304dd
Jonas Auer <jonas.auer.94@gmail.com>
2017-11-16 11:43:00 +0100

    Add a restart button to the settings menu Fix #178

--- src/overlays/settings.ts
+++ src/overlays/settings.ts
@@ -2,6 +2,8 @@ import { Button, ButtonState } from '../gameObjects/button'
 import { Atlases, Images } from '../assets'
 import Slider from '../gameObjects/slider'
 
+import RestartOverlay from './restart'
+
 import { AudioManager } from '../utils/audioManager'
 
 export default class SettingsOverlay extends Phaser.Group {
@@ -46,57 +48,79 @@ export default class SettingsOverlay extends Phaser.Group {
     backgroundShading.endFill()
     this.add(backgroundShading)
 
+    const centerX = Math.floor(this.game.width / 2)
+    const centerY = Math.floor(this.game.height / 2)
+
+    const backgroundWidth = 150
+    const backgroundHeight = 140
+
+    const backgroundLeft = Math.floor(centerX - backgroundWidth / 2)
+    const backgroundTop = Math.floor(centerY - backgroundHeight / 2)
+    const backgroundBottom = Math.floor(centerY + backgroundHeight / 2)
+
     this.addMultiple([
-      new Phaser.Sprite(game, 117, 46, Atlases.settingsBackground.key, Atlases.settingsBackground.frames.top),
-      new Phaser.TileSprite(game, 117, 48, 150, 118, Atlases.settingsBackground.key, Atlases.settingsBackground.frames.center),
-      new Phaser.Sprite(game, 117, 166, Atlases.settingsBackground.key, Atlases.settingsBackground.frames.bottom)
+      new Phaser.Sprite(game, backgroundLeft, backgroundTop, Atlases.settingsBackground.key, Atlases.settingsBackground.frames.top),
+      new Phaser.TileSprite(game, backgroundLeft, backgroundTop + 2, backgroundWidth, backgroundHeight - 5, Atlases.settingsBackground.key, Atlases.settingsBackground.frames.center),
+      new Phaser.Sprite(game, backgroundLeft, backgroundBottom - 3, Atlases.settingsBackground.key, Atlases.settingsBackground.frames.bottom)
     ])
 
-    const confirmButton = new Button(this.game, 157, 154, Atlases.button.key)
+    const confirmButton = new Button(this.game, centerX - 35, backgroundBottom - 16, Atlases.button.key)
     confirmButton.events.onInputUp.add(() => {
       this.hide(false)
     })
-    const checkmarkIcon = new Phaser.Sprite(this.game, 152, 149, Images.iconsCheckmark.key)
+    const checkmarkIcon = new Phaser.Sprite(this.game, centerX - 40, backgroundBottom - 21, Images.iconsCheckmark.key)
     confirmButton.onStateChanged.add(state => {
-      checkmarkIcon.y = state === ButtonState.DOWN ? 150 : 149
+      checkmarkIcon.y = backgroundBottom - (state === ButtonState.DOWN ? 20 : 21)
     })
     this.add(confirmButton)
     this.add(checkmarkIcon)
 
-    const abortButton = new Button(this.game, 227, 154, Atlases.button.key)
+    const abortButton = new Button(this.game, centerX + 35, backgroundBottom - 16, Atlases.button.key)
     abortButton.events.onInputUp.add(() => {
       this.hide(true)
     })
-    const crossIcon = new Phaser.Sprite(this.game, 223, 149, Images.iconsCross.key)
+    const crossIcon = new Phaser.Sprite(this.game, centerX + 31, backgroundBottom - 21, Images.iconsCross.key)
     abortButton.onStateChanged.add(state => {
-      crossIcon.y = state === ButtonState.DOWN ? 150 : 149
+      crossIcon.y = backgroundBottom - (state === ButtonState.DOWN ? 20 : 21)
     })
     this.add(abortButton)
     this.add(crossIcon)
 
+    const restartButton = new Button(this.game, centerX, backgroundTop + 103, Atlases.wideButton.key)
+    restartButton.events.onInputUp.add(() => {
+      this.hide()
+      RestartOverlay.instance.show()
+    })
+    const restartIcon = new Phaser.Sprite(this.game, centerX - 3, backgroundTop + 98, Images.iconsRestart.key)
+    restartButton.onStateChanged.add(state => {
+      restartIcon.y = backgroundTop + (state === ButtonState.DOWN ? 99 : 98)
+    })
+    this.add(restartButton)
+    this.add(restartIcon)
+
     this.masterVolumeSlider = new Slider(this.game, Images.iconsSpeaker.key)
-    this.masterVolumeSlider.position.setTo(126, 55)
+    this.masterVolumeSlider.position.setTo(centerX - 66, backgroundTop + 8)
     this.masterVolumeSlider.onValueChanged.add(value => {
       AudioManager.instance.master = value
     })
     this.add(this.masterVolumeSlider)
 
     this.musicVolumeSlider = new Slider(this.game, Images.iconsMusic.key)
-    this.musicVolumeSlider.position.setTo(126, 78)
+    this.musicVolumeSlider.position.setTo(centerX - 66, backgroundTop + 30)
     this.musicVolumeSlider.onValueChanged.add(value => {
       AudioManager.instance.tracks.music.volume = value
     })
     this.add(this.musicVolumeSlider)
 
     this.atmoVolumeSlider = new Slider(this.game, Images.iconsAtmo.key)
-    this.atmoVolumeSlider.position.setTo(126, 101)
+    this.atmoVolumeSlider.position.setTo(centerX - 66, backgroundTop + 52)
     this.atmoVolumeSlider.onValueChanged.add(value => {
       AudioManager.instance.tracks.atmo.volume = value
     })
     this.add(this.atmoVolumeSlider)
 
     this.speechVolumeSlider = new Slider(this.game, Images.iconsSpeech.key)
-    this.speechVolumeSlider.position.setTo(126, 124)
+    this.speechVolumeSlider.position.setTo(centerX - 66, backgroundTop + 74)
     this.speechVolumeSlider.onValueChanged.add(value => {
       AudioManager.instance.tracks.speech.volume = value
     })
@@ -134,7 +158,7 @@ export default class SettingsOverlay extends Phaser.Group {
   }
 
   public hide(resetSettings: boolean = false) {
-    if (resetSettings) {
+    if (resetSettings && this.oldState) {
       this.masterVolumeSlider.value = this.oldState.masterVolume
       this.musicVolumeSlider.value = this.oldState.musicVolume
       this.atmoVolumeSlider.value = this.oldState.atmoVolume
