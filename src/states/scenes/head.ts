f9cbca61b9333d6b745133975173068f76de842e
Jonas Auer <jonas.auer.94@gmail.com>
2017-12-04 17:50:51 +0100

    Remove outdated TODO

--- src/states/scenes/head.ts
+++ src/states/scenes/head.ts
@@ -138,9 +138,6 @@ export default class HeadScene extends Scene {
       new ConditionalStateTransition(
         Suction,
         TransitionCondition.reachedState(this.stateManagers.painter, PainterIsDoneWithPainting)
-        // TODO: add conditions for minor parallel storylines
-        // TransitionCondition.reachedState(scenes.bard.stateManagers.bard, BardGone)
-        // .and(TransitionCondition.reachedState(scenes.bard.stateManagers.meckie, MeckieGone))
       )
     )
 
